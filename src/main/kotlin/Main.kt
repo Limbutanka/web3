de839d8dd951d1957ac2d051c383f508055ce759
tankaraj <H@>
2023-10-17 11:58:54 +0900

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,24 @@
 fun main() {
-    val add = 6 + 2
-    println(add) // 8
+    val add = 10 + 12
+    println(add) // 22
 
-    val minus = 6 - 2
-    println(minus) // 4
+    val minus = 66 - 22
+    println(minus) // 44
 
-    val mul = 6 * 2
-    println(mul) // 12
+    val mul = 16 * 2
+    println(mul) // 32
 
-    val divide = 6 / 2
+    val divide = 36 / 12
     println(divide) // 3
 
-    val divide2 = 7 / 2
-    println(divide2) // 3 (!)
+    val divide2 = 18 / 2
+    println(divide2) // 9 (!)
 
-    val divide3 = 8 / 2
+    val divide3 = 28 / 7
     println(divide3) // 4
 
-    val divide4 = 7 / 2.0
-    println(divide4) // 3.5 (!)
+    val divide4 = 30 / 2.0
+    println(divide4) // 15 (!)
 
     val remainder = 7 % 2
     println(remainder) // 1
